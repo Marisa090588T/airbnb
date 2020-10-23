e37a8bc2782abfe945c6d549b87abbc516624eb5
Marisa Tsubokura <marieee090530@gmail.com>
2020-10-23 14:58:43 +0900

    created booking controller and model

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,6 @@
 class DashboardsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+    @requests = Booking.all.select {|booking| booking.accommodation.user == current_user }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     resources :bookings
   end
 
-  resources :bookings
+  # resources :bookings
 end
