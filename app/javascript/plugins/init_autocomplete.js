bb2347ae0f6683ab04416688bedc2b96bd5cddaa
Marisa Tsubokura <marieee090530@gmail.com>
2020-10-24 14:08:53 +0900

    fixed map (should be fixed)

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,14 +1,15 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('flat_address');
-  if (addressInput) {
-    places({ container: addressInput });
+  const addressInput = document.getElementById('accommodation_location');
+  if (locationInput) {
+    places({ container: locationInput });
   }
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb' // <-- use your own!
-  });
 };
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v10'
+});
 
 export { initAutocomplete };
