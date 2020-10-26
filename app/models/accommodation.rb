8c2ab4f75852b7da7c2b8c2af78924fd29231dc4
Marisa Tsubokura <marieee090530@gmail.com>
2020-10-26 09:54:26 +0900

    front for show pages

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -2,6 +2,7 @@ class Accommodation < ApplicationRecord
   TYPES = ["Family", "Business", "Single", "other"]
   belongs_to :user
   has_one_attached :photo
+  has_one :booking
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :price, presence: true
